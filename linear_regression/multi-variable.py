2781e23435f685577a324c5c6ebad88ac2882f2f
Ali Raza Khalid <razamindset.official@gmail.com>
2025-08-15 20:59:16 +0500

    Fix: errors

--- linear_regression/multi-variable.py
+++ linear_regression/multi-variable.py
@@ -19,11 +19,11 @@ def multi_input_gradient_decent(m, b, points, lr):
         # M is also a row vector where colums are equal to the number of features
         # the dot prodeuct will do the follwoign calculation y_pred = h(x) = m1x1 + m2x2 + ... + b
         y_pred = np.dot(m, X) + b
-        error = (y - y_pred) ** 2
+        error = (y - y_pred)
 
         # Calculate the gradients using the formaula for mse
-        m_grad = -(2/n) * X * error
-        b_grad = -(2/n) * error
+        m_grad += -(2/n) * X * error
+        b_grad += -(2/n) * error
     
     m = m - lr * m_grad
     b = b - lr * b_grad
@@ -44,3 +44,12 @@ for i in range(epochs):
     m, b = multi_input_gradient_decent(m, b, df, lr)
 
 print(m,b)
+
+y_pred = df[['Hours_Study', 'Sleep_Hours']].values @ m + b
+plt.scatter(df.Score, y_pred, color='blue')
+plt.xlabel("Actual Score")
+plt.ylabel("Predicted Score")
+plt.plot([df.Score.min(), df.Score.max()],
+         [df.Score.min(), df.Score.max()],
+         color='red', linestyle='--')
+plt.show()
