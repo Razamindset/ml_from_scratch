8ebab7ba073a3aed742248ba8d2695c119f497b8
Razamindset <razamindset.official@gmail.com>
2025-10-29 14:12:22 +0500

    Add: Svm init

---
+++ svm/SVM.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class SVM:
+    def __init__(self, learning_rate=1e-3, lambda_param=1e-2, n_iters=1000):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
+
